02ab8396760c5f8c49f1f70974b0c33573a77d13
Ranajoy <rsadhukh@login-gpu.cm.cluster>
2024-07-08 18:58:33 -0400

    updated download.py

--- latency.py
+++ latency.py
@@ -304,7 +304,6 @@ def main(
                     model,
                     encoded,
                     decode_length,
-                    device,
                     temperature=temperature,
                     top_k=top_k,
                 )        
